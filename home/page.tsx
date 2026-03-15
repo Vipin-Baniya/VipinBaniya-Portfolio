2ed865c9e4fda0bee59dff2758f84ce261e14f5d
Vipin Baniya <Vips2004baniya@gmail.com>
2026-03-16 00:35:54 +0530

    Delete home directory

--- home/page.tsx
+++
@@ -1,5 +0,0 @@
-import PublicLayout from "@/components/layout/PublicLayout";
-import HomeView from "@/components/public/HomeView";
-export default function HomePage() {
-  return <PublicLayout active="home"><HomeView /></PublicLayout>;
-}
